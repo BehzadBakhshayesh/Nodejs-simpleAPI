becb5cf12b8843c20ca09cec7fd13a85b300220e
Bhzd <bhzd.bakhshayesh@gmail.com>
2024-09-15 15:47:56 +0430

    node jssimple api and server

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/") {
+    res.end("this is ROOT");
+  } else if (pathName === "/product") {
+    res.end("this is PRODUCT");
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/data.json`, "utf-8", (err, data) => {
+      //   const fileData = JSON.parse(data);
+      res.writeHead(200, {
+        "Content-type": "application/json",
+      });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>PAGE NOT FOUND</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listen on port 8000");
+});
